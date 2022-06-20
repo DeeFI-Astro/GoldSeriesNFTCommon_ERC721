29c5b9d983eda71b359d1856e24936e5ca9840da
"Bryce Shabazz" <"deefiastro@gmail.com">
2022-06-20 06:12:30 -0500

    GoldSeriesCommonNFT

---
+++ contract-3171add437.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+
+contract GoldSeriesNFTCommon is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("GoldSeriesNFTCommon", "GSC") {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
